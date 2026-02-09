22b13b6e59db3483a48e36bb5710965907ce05a7
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-10 00:30:56 +0600

    refactored  ramcard code

--- src/component/teamCard/TeamCard.jsx
+++ src/component/teamCard/TeamCard.jsx
@@ -1,35 +1,49 @@
 import { Image } from "react-bootstrap";
 import "./teamCard.css";
 
-// import for icon
+// Import icons
 import { FaPlus } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 import { IoLogoLinkedin } from "react-icons/io5";
 import { FaPinterest } from "react-icons/fa6";
 
+// Import team image
 import ImageOne from "../../assets/teamcard1.png";
 
 const TeamCard = ({ heading, paragraph }) => {
   return (
     <div className="teamCardElement">
+      {/* Team Image */}
       <Image src={ImageOne} className="teamCardImages" />
+
+      {/* Team Text */}
       <div className="teamCardText">
-        <div className="teamCardLine"></div>
+        <div className="teamCardLine" />
+
         <div className="teamCardTitle">
           <h6>{heading}</h6>
           <p>{paragraph}</p>
         </div>
       </div>
+
+      {/* Social / Action Icons */}
       <div className="teamCardIcon">
+        {/* Plus Icon */}
         <div className="teamCardIconOne teamCardCommonCss">
           <FaPlus className="teamCardCommon crossIcon" />
         </div>
+
+        {/* Twitter Icon */}
         <div className="teamCardIconTwo teamCardCommonCss">
-          <FaTwitter className="teamCardCommon  teamCardOtherIcon " />
+          <FaTwitter className="teamCardCommon teamCardOtherIcon" />
         </div>
+
+        {/* LinkedIn Icon */}
         <div className="teamCardIconThree teamCardCommonCss">
           <IoLogoLinkedin className="teamCardCommon teamCardOtherIcon" />
         </div>
+
+        {/* Pinterest Icon */}
         <div className="teamCardIconFour teamCardCommonCss">
           <FaPinterest className="teamCardCommon teamCardOtherIcon" />
         </div>
