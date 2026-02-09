b2bfdf0045f3e08e09bbe34608b25a7c4dacc35e
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-10 00:27:51 +0600

    refactored this code of solution part

--- src/component/solution/Solution.jsx
+++ src/component/solution/Solution.jsx
@@ -2,7 +2,7 @@ import { Col, Container, Image, Row } from "react-bootstrap";
 import "./solution.css";
 import Button from "../commonButton/CommonButtom";
 
-// import for images
+// Import images
 import SolutionImgOne from "../../assets/solution2.png";
 import SolutionImgTwo from "../../assets/solution3.png";
 import SolutionImgThree from "../../assets/solution1.png";
@@ -15,28 +15,35 @@ const Solution = () => {
     <section className="solution">
       <Container>
         <Row>
+          {/* Left Part */}
           <Col lg={{ span: 5 }}>
             <div className="solutionLeftPart">
               <h2 className="solutionLeftPartHeadingOne">
                 Best Reliable Solution For Your Business
               </h2>
+
               <p className="solutionLeftPartParagraphOne">
                 We help our clients succeed by creating brand identities,
                 digital experiences, and print materials that communicate
                 clearly, achieve marketing.
               </p>
-              <Button buttonName={"Get Started Now"} />
+
+              <Button buttonName="Get Started Now" />
             </div>
+
             <div className="solutionLeftPartBottom">
+              {/* Bottom Element 1 */}
               <div className="solutionLeftPartBottomElement">
                 <h5 className="solutionLeftPartHeadingTwo">
                   First-class investment solutions
                 </h5>
                 <p className="solutionLeftPartParagraphTwo">
                   We can help you with your business strategy, architecture,
-                  mapping, and optimisation.{" "}
+                  mapping, and optimisation.
                 </p>
               </div>
+
+              {/* Bottom Element 2 */}
               <div className="solutionLeftPartBottomElement">
                 <h5 className="solutionLeftPartHeadingTwo">
                   Building the idea of future
@@ -48,20 +55,25 @@ const Solution = () => {
               </div>
             </div>
           </Col>
+
+          {/* Right Part */}
           <Col lg={{ span: 6, offset: 1 }}>
             <div className="solutionRightPart">
+              {/* Animated Images */}
               <Image src={SolutionImgOne} className="imageSolOne" />
               <Image src={SolutionImgTwo} className="imageSolTwo" />
               <Image src={SolutionImgThree} className="imageSolThree" />
               <Image src={SolutionImgFour} className="imageSolFoures" />
               <Image src={SolutionImgFive} className="imageSolFive" />
               <Image src={SolutionImgSix} className="imageSolSix" />
-              
 
+              {/* Experience Card */}
               <div className="exprienceCard">
                 <h1 className="exprienceCardHeadingOne">24</h1>
-                <h5 className="exprienceCardHeadingTwo">Years Experience</h5>
-                <div className="exCardLine"></div>
+                <h5 className="exprienceCardHeadingTwo">
+                  Years Experience
+                </h5>
+                <div className="exCardLine" />
               </div>
             </div>
           </Col>
