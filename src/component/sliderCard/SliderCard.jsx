53f75c8b7df8dc25edad06a8e89a2da7210b0547
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-10 00:13:39 +0600

    rechek slider card code clean & developer friendly

--- src/component/sliderCard/SliderCard.jsx
+++ src/component/sliderCard/SliderCard.jsx
@@ -10,15 +10,26 @@ const SliderCard = ({
   sliderParentBg,
 }) => {
   return (
-    <div className="sliderCardMain" style={{ backgroundColor: sliderParentBg }}>
+    <div
+      className="sliderCardMain"
+      style={{ backgroundColor: sliderParentBg }}
+    >
       <div className="sliderCardElement">
+        {/* Card Image */}
         <Image src={srcImage} className="sliderImage" />
-        <div className="sliderParent" style={{ borderColor: borderColor }}>
+
+        {/* Animated Circle with Dot */}
+        <div
+          className="sliderParent"
+          style={{ borderColor: borderColor }}
+        >
           <div
             className="sliderDot"
             style={{ backgroundColor: dotBackground }}
-          ></div>
+          />
         </div>
+
+        {/* Card Text */}
         <h6 className="sliderCardHeading">{heading}</h6>
         <p className="sliderCardParagraph">{paragraph}</p>
       </div>
