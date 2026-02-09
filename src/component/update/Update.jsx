8da49c5dd1c60e031fbdddf2ca2f322182f35bbe
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-10 00:32:09 +0600

    refactored code of update part

--- src/component/update/Update.jsx
+++ src/component/update/Update.jsx
@@ -1,14 +1,17 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./update.css";
 import UpdateCard from "../updateCard/UpdateCard";
-// import for image
+
+// Import images
 import Nature from "../../assets/nature.webp";
 import NatureTwo from "../../assets/nature2.avif";
 import NatureThree from "../../assets/nature3.jpg";
+
 const Update = () => {
   return (
     <section className="updateMain">
       <Container>
+        {/* Top Heading */}
         <Row>
           <Col lg={12}>
             <div className="updateTopPart">
@@ -19,41 +22,39 @@ const Update = () => {
             </div>
           </Col>
         </Row>
+
+        {/* Update Cards */}
         <Row>
           <Col lg={4}>
             <UpdateCard
-              heading={"Simple Juice Recipes to boost your immune system"}
-              paragraph={
-                "Objectively restore stand-alone markets rather than enterprise-wide products. "
-              }
+              heading="Simple Juice Recipes to boost your immune system"
+              paragraph="Objectively restore stand-alone markets rather than enterprise-wide products."
               imageSrc={Nature}
               personImage={Nature}
-              personName={"Esther Howard"}
-              dateText={"8 Sep, 2021"}
+              personName="Esther Howard"
+              dateText="8 Sep, 2021"
             />
           </Col>
+
           <Col lg={4}>
             <UpdateCard
-              heading={"Simple Juice Recipes to boost your immune system"}
-              paragraph={
-                "Objectively restore stand-alone markets rather than enterprise-wide products. "
-              }
+              heading="Simple Juice Recipes to boost your immune system"
+              paragraph="Objectively restore stand-alone markets rather than enterprise-wide products."
               imageSrc={NatureTwo}
               personImage={NatureTwo}
-              personName={"Esther Howard"}
-              dateText={"8 Sep, 2021"}
+              personName="Esther Howard"
+              dateText="8 Sep, 2021"
             />
           </Col>
+
           <Col lg={4}>
             <UpdateCard
-              heading={"Simple Juice Recipes to boost your immune system"}
-              paragraph={
-                "Objectively restore stand-alone markets rather than enterprise-wide products. "
-              }
+              heading="Simple Juice Recipes to boost your immune system"
+              paragraph="Objectively restore stand-alone markets rather than enterprise-wide products."
               imageSrc={NatureThree}
               personImage={NatureThree}
-              personName={"Esther Howard"}
-              dateText={"8 Sep, 2021"}
+              personName="Esther Howard"
+              dateText="8 Sep, 2021"
             />
           </Col>
         </Row>
