368e3fea107f610aa8d6f8ca434a10b367b3e7b2
Mostofa-Zaman <mostofazaman1234@gmail.com>
2026-02-10 00:29:16 +0600

    refactored this code of teem part

--- src/component/team/Team.jsx
+++ src/component/team/Team.jsx
@@ -1,15 +1,19 @@
 import { Col, Container, Image, Row } from "react-bootstrap";
 import "./team.css";
 
-// import for images
+// Import images
 import Moon from "../../assets/halfmoon.png";
 import Lases from "../../assets/longlash.png";
+
+// Import TeamCard component
 import TeamCard from "../teamCard/TeamCard";
 
 const Team = () => {
   return (
     <>
+      {/* Team Section One - Header & Animations */}
       <section className="teamOneMain">
+        {/* Background Animations */}
         <Image src={Moon} className="moonAnimation" />
         <Image src={Lases} className="lasesAnimation" />
 
@@ -24,39 +28,36 @@ const Team = () => {
           </Row>
         </Container>
       </section>
+
+      {/* Team Section Two - Team Cards */}
       <section className="teamTwoMain">
         <Container>
           <Row>
+            {/* Team Member 1 */}
             <Col lg={3}>
               <div className="teamTwoElement">
-                <TeamCard
-                  heading={"Eleanor Pena"}
-                  paragraph={"Senior Engineer"}
-                />
+                <TeamCard heading="Eleanor Pena" paragraph="Senior Engineer" />
               </div>
             </Col>
+
+            {/* Team Member 2 */}
             <Col lg={3}>
               <div className="teamTwoElement">
-                <TeamCard
-                  heading={"Jenny Wilson"}
-                  paragraph={"Project Manager"}
-                />
+                <TeamCard heading="Jenny Wilson" paragraph="Project Manager" />
               </div>
             </Col>
+
+            {/* Team Member 3 */}
             <Col lg={3}>
               <div className="teamTwoElement">
-                <TeamCard
-                  heading={"Jerome Bell"}
-                  paragraph={"Senior Engineer"}
-                />
+                <TeamCard heading="Jerome Bell" paragraph="Senior Engineer" />
               </div>
             </Col>
+
+            {/* Team Member 4 */}
             <Col lg={3}>
               <div className="teamTwoElement">
-                <TeamCard
-                  heading={"Robert Fox"}
-                  paragraph={"Marketing Expert"}
-                />
+                <TeamCard heading="Robert Fox" paragraph="Marketing Expert" />
               </div>
             </Col>
           </Row>
